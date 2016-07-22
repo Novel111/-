d73a61f1e576c587cd6938d02f98d3e7871a2855
汪宇豪 <462775767@qq.com>
2016-07-22 09:57:40 +0800

    Initial Commit

---
+++ 十二码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  十二码
+//
+//  Created by 汪宇豪 on 16/7/22.
+//  Copyright © 2016年 汪宇豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
